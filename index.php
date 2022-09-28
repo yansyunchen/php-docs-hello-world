a896a71ac2a57a51be2c1d13d042f301ff0c16b0
yansyunchen <f128909090@hotmail.com>
2022-09-28 13:50:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 print_r(get_loaded_extensions());
 $upload_max_size = ini_get('upload_max_filesize');
-print $upload_max_size;
+print $upload_max_size\n;
 ini_set('upload_max_filesize', '500M');
-print " " + $upload_max_size;
+print $upload_max_size;
 // php -r '$all = get_loaded_extensions(); foreach($all as $i) { $ext = new ReflectionExtension($i); $ver = $ext->getVersion(); echo "$i - $ver" . PHP_EOL;}'
 ?>
